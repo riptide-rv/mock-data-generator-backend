be8394fee2e33e4384bfbb433b8ff676a53d42c3
Rohit Ramdas V <rohitramdas2003@gmail.com>
2024-07-22 23:37:42 +0530

    Code Refactor - change typing List to python list in ProjectBase

--- src/models/Project.py
+++ src/models/Project.py
@@ -1,14 +1,14 @@
 from models.Field import FieldDetails
 from pydantic import BaseModel
 from uuid import UUID
-from typing import List
+
 
 class ProjectBase(BaseModel):
     id: UUID 
     name: str
     description: str
     owner_id: UUID 
-    fields: List[FieldDetails] | None = None 
+    fields: list[FieldDetails] = []
 
 class ProjectCreate(BaseModel):
     name: str
