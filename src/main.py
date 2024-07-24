207f0ac0bf777b6a47dbfa6c3e5994a865e6f861
Rohit Ramdas V <rohitramdas2003@gmail.com>
2024-07-24 21:42:15 +0530

    Added cors permission for frontend

--- src/main.py
+++ src/main.py
@@ -3,13 +3,24 @@ from controllers.UserController import router as user_router
 from controllers.ProjectController import router as project_router
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import models.model as model
 from repositories.config import engine
 
 
-
+origins = {
+    "http://localhost",
+    "http://localhost:3000",
+}   
 
 app = FastAPI()
+app.add_middleware(
+   CORSMiddleware,
+    allow_origins = origins,
+    allow_credentials =True,
+    allow_methods = ["*"],
+    allow_headers= ["*"],
+)
 app.include_router(auth_router)
 app.include_router(user_router)
 app.include_router(project_router)
